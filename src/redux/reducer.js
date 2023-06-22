a4d13d3379cbb05f95590d3d79e598bb02577c36
LeviTC <erletaco@outlook.com>
2023-06-22 00:42:12 -0600

    Agregando boton de favorito con redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,29 @@
+import { ADD_FAV, REMOVE_FAV } from "./type_action.js";
+
+
+
+const initialState = {
+    myFavorites: [],
+
+}
+
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_FAV:
+            return {
+                ...state,
+                myFavorites: [...state.myFavorites, action.payload]
+            };
+        case REMOVE_FAV:
+            return {
+                ...state,
+                myFavorites: state.myFavorites.filter(character => character.id !== action.payload)
+            };
+
+        default:
+            return { ...state };
+    }
+}
+
+export default rootReducer;
---
+++ src/redux/type_action.js
@@ -0,0 +1,3 @@
+
+export const REMOVE_FAV = "REMOVE_FAV";
+export const ADD_FAV = "ADD_FAV";
